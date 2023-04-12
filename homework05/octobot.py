e585fd299695ed381cdf8b78470957ca8c84a83a
Daria13-gif <dpolakova27@gmail.com>
2023-04-12 19:43:24 +0300

    fix

--- homework05/octobot.py
+++ homework05/octobot.py
@@ -1,11 +1,11 @@
 import json
+from datetime import datetime
+from time import sleep
+
 import gspread
 import pandas as pd
 import telebot
 import validators
-
-from datetime import datetime
-from time import sleep
 
 
 bot = telebot.TeleBot("6055195312:AAHUl2VodKXqqT6YxNmd-E16Fg79wxphXLo")
