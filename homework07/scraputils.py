f7d1e419587afc2a6669a651e5f32505f0f87d4b
Daria13-gif <124306554+Daria13-gif@users.noreply.github.com>
2023-05-20 11:41:45 +0300

    fix

--- homework07/scraputils.py
+++ homework07/scraputils.py
@@ -61,7 +61,7 @@ def extract_news(parser):
             # (на это указывает наличие класса "hnuser" в ссылке в строке),
             # функция извлекает имя автора, количество баллов, полученных статьей,
             # и количество комментариев, полученных статьей, и сохраняет их в словаре ninf.
-            if n is not None:
+            if n.find("a") is not None:
                 if n.find("a").attrs:
                     if "class" in n.find("a").attrs and n.find("a").attrs["class"][0] == "hnuser":
                         ninf["author"] = n.find("a").string
