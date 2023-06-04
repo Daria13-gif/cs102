4088233a5c6e9d557453f23bcb0e9f76189fbe9c
Daria13-gif <124306554+Daria13-gif@users.noreply.github.com>
2023-06-04 20:57:31 +0300

    fix

---
+++ homework07/vkapi/__init__.py
@@ -0,0 +1,4 @@
+from homework07.vkapi.config import VK_CONFIG
+from homework07.vkapi.session import Session
+
+session = Session(VK_CONFIG["domain"])
---
+++ homework07/vkapi/config.py
@@ -0,0 +1,7 @@
+# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+
+VK_CONFIG = {
+    "domain": "https://api.vk.com/method",
+    "access_token": "vk1.a.btZHaMV_1eIGkJzo-GnG9mZf6q9SnsHerCpVqjuDMdvhcwAvT3HPo9Ofc0lwI3qkueUmv__FGNoYNwMnbOQpS1bZXfTqhJqZilC6Zc0PpUAjtZFSvWnAFUCdQV7EAEr8-YzoH0jqwGbJ8giFPrklnIg9x15z1vVq8Pus88A8WviypWGgLdv1-K-VjmItO3D6",
+    "version": "5.126",
+}
