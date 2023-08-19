15ebb979fa7d9353a495c91ff430bc1d693cf615
Daria13-gif <124306554+Daria13-gif@users.noreply.github.com>
2023-08-19 15:46:04 +0300

    fix

--- homework03/maze.py
+++ homework03/maze.py
@@ -9,9 +9,7 @@ def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
     return [["■"] * cols for _ in range(rows)]
 
 
-def remove_wall(
-    grid: List[List[Union[str, int]]], coord: Tuple[int, int]
-) -> List[List[Union[str, int]]]:
+def remove_wall(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) -> List[List[Union[str, int]]]:
     """
 
     :param grid:
@@ -28,9 +26,7 @@ def remove_wall(
     return grid
 
 
-def bin_tree_maze(
-    rows: int = 15, cols: int = 15, random_exit: bool = True
-) -> List[List[Union[str, int]]]:
+def bin_tree_maze(rows: int = 15, cols: int = 15, random_exit: bool = True) -> List[List[Union[str, int]]]:
     """
 
     :param rows:
@@ -192,10 +188,10 @@ def encircled_exit(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) ->
     ans = False
 
     if (
-            coord == (0, 0)
-            or coord == (len(grid) - 1, len(grid[0]) - 1)
-            or coord == (len(grid) - 1, 0)
-            or coord == (0, len(grid[0]) - 1)
+        coord == (0, 0)
+        or coord == (len(grid) - 1, len(grid[0]) - 1)
+        or coord == (len(grid) - 1, 0)
+        or coord == (0, len(grid[0]) - 1)
     ):
         ans = True
     elif coord[0] == 0:
