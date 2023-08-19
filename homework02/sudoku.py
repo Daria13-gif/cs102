aac566974db24de09faafb3ece52f5f4c3c457cf
Daria13-gif <124306554+Daria13-gif@users.noreply.github.com>
2023-08-19 15:51:39 +0300

    fix

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,284 +1,243 @@
-from copy import deepcopy
-from random import choice, randint
-from typing import List, Optional, Tuple, Union
+import unittest
 
-import pandas as pd
+import sudoku
 
 
-def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
-    return [["■"] * cols for _ in range(rows)]
+class SudokuTestCase(unittest.TestCase):
+    def test_group(self):
+        values = [1, 2, 3, 4]
+        expected_groups = [[1, 2], [3, 4]]
+        actual_groups = sudoku.group(values, 2)
+        self.assertEqual(expected_groups, actual_groups)
 
+        values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        expected_groups = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        actual_groups = sudoku.group(values, 3)
+        self.assertEqual(expected_groups, actual_groups)
 
-def remove_wall(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) -> List[List[Union[str, int]]]:
-    """
+    def test_get_row(self):
+        puzzle = [["1", "2", "."], ["4", "5", "6"], ["7", "8", "9"]]
+        pos = (0, 0)
+        expected_row = ["1", "2", "."]
+        actual_row = sudoku.get_row(puzzle, pos)
+        self.assertEqual(expected_row, actual_row)
 
-    :param grid:
-    :param coord:
-    :return:
-    """
+        puzzle = [["1", "2", "3"], ["4", ".", "6"], ["7", "8", "9"]]
+        pos = (1, 0)
+        expected_row = ["4", ".", "6"]
+        actual_row = sudoku.get_row(puzzle, pos)
+        self.assertEqual(expected_row, actual_row)
 
-    if grid[coord[0]][coord[1]] != " ":
-        grid[coord[0]][coord[1]] = " "
-    elif coord[1] + 1 < len(grid[0]) - 1:
-        grid[coord[0]][coord[1] + 1] = " "
-    elif coord[0] - 1 > 1:
-        grid[coord[0] - 1][coord[1]] = " "
-    return grid
+        puzzle = [["1", "2", "3"], ["4", "5", "6"], [".", "8", "9"]]
+        pos = (2, 0)
+        expected_row = [".", "8", "9"]
+        actual_row = sudoku.get_row(puzzle, pos)
+        self.assertEqual(expected_row, actual_row)
 
+    def test_get_col(self):
+        puzzle = [["1", "2", "."], ["4", "5", "6"], ["7", "8", "9"]]
+        pos = (0, 0)
+        expected_col = ["1", "4", "7"]
+        actual_col = sudoku.get_col(puzzle, pos)
+        self.assertEqual(expected_col, actual_col)
 
-def bin_tree_maze(rows: int = 15, cols: int = 15, random_exit: bool = True) -> List[List[Union[str, int]]]:
-    """
+        puzzle = [["1", "2", "3"], ["4", ".", "6"], ["7", "8", "9"]]
+        pos = (0, 1)
+        expected_col = ["2", ".", "8"]
+        actual_col = sudoku.get_col(puzzle, pos)
+        self.assertEqual(expected_col, actual_col)
 
-    :param rows:
-    :param cols:
-    :param random_exit:
-    :return:
-    """
+        puzzle = [["1", "2", "3"], ["4", "5", "6"], [".", "8", "9"]]
+        pos = (0, 2)
+        expected_col = ["3", "6", "9"]
+        actual_col = sudoku.get_col(puzzle, pos)
+        self.assertEqual(expected_col, actual_col)
 
-    grid = create_grid(rows, cols)
-    empty_cells = []
-    for x, row in enumerate(grid):
-        for y, _ in enumerate(row):
-            if x % 2 == 1 and y % 2 == 1:
-                grid[x][y] = " "
-                empty_cells.append((x, y))
+    def test_get_block(self):
+        grid = [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
 
-    # 1. выбрать любую клетку
-    # 2. выбрать направление: наверх или направо.
-    # Если в выбранном направлении следующая клетка лежит за границами поля,
-    # выбрать второе возможное направление
-    # 3. перейти в следующую клетку, сносим между клетками стену
-    # 4. повторять 2-3 до тех пор, пока не будут пройдены все клетки
+        pos = (0, 1)
+        expected_block = ["5", "3", ".", "6", ".", ".", ".", "9", "8"]
+        actual_block = sudoku.get_block(grid, pos)
+        self.assertEqual(expected_block, actual_block)
 
-    # генерация входа и выхода
-    random_action = [-1, 1]
-    for row_cor in range(1, rows - 1, 2):
-        for col_cor in range(1, cols - 1, 2):
-            action = choice(random_action)
-            if action == 1:
-                if row_cor == 1:
-                    if col_cor + 1 == cols - 1:
-                        continue
-                    remove_wall(grid, (row_cor, col_cor + 1))
-                elif col_cor + 1 < cols - 1:
-                    remove_wall(grid, (row_cor, col_cor + 1))
-                elif col_cor - 1 <= cols - 1:
-                    remove_wall(grid, (row_cor - 1, col_cor))
-            else:
-                if row_cor == 1:
-                    if col_cor + 1 == cols - 1:
-                        continue
-                    remove_wall(grid, (row_cor, col_cor + 1))
-                elif row_cor + 1 <= rows - 1:
-                    remove_wall(grid, (row_cor - 1, col_cor))
+        pos = (4, 7)
+        expected_block = [".", ".", "3", ".", ".", "1", ".", ".", "6"]
+        actual_block = sudoku.get_block(grid, pos)
+        self.assertEqual(expected_block, actual_block)
 
-    if random_exit:
-        x_in, x_out = randint(0, rows - 1), randint(0, rows - 1)
-        y_in = randint(0, cols - 1) if x_in in (0, rows - 1) else choice((0, cols - 1))
-        y_out = randint(0, cols - 1) if x_out in (0, rows - 1) else choice((0, cols - 1))
-    else:
-        x_in, y_in = 0, cols - 2
-        x_out, y_out = rows - 1, 1
+        pos = (8, 8)
+        expected_block = ["2", "8", ".", ".", ".", "5", ".", "7", "9"]
+        actual_block = sudoku.get_block(grid, pos)
+        self.assertEqual(expected_block, actual_block)
 
-    grid[x_in][y_in], grid[x_out][y_out] = "X", "X"
+    def test_find_empty_positions(self):
+        grid = [["1", "2", "."], ["4", "5", "6"], ["7", "8", "9"]]
+        expected_pos = (0, 2)
+        actual_pos = sudoku.find_empty_positions(grid)
+        self.assertEqual(expected_pos, actual_pos)
 
-    return grid
+        grid = [["1", "2", "3"], ["4", ".", "6"], ["7", "8", "9"]]
+        expected_pos = (1, 1)
+        actual_pos = sudoku.find_empty_positions(grid)
+        self.assertEqual(expected_pos, actual_pos)
 
+        grid = [["1", "2", "3"], ["4", "5", "6"], [".", "8", "9"]]
+        expected_pos = (2, 0)
+        actual_pos = sudoku.find_empty_positions(grid)
+        self.assertEqual(expected_pos, actual_pos)
 
-def get_exits(grid: List[List[Union[str, int]]]) -> List[Tuple[int, int]]:
-    """
+    def test_find_possible_values(self):
+        grid = [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+        pos = (0, 2)
+        expected_values = {"1", "2", "4"}
+        actual_values = sudoku.find_possible_values(grid, pos)
+        self.assertEqual(expected_values, actual_values)
 
-    :param grid:
-    :return:
-    """
+        pos = (4, 7)
+        expected_values = {"2", "5", "9"}
+        actual_values = sudoku.find_possible_values(grid, pos)
+        self.assertEqual(expected_values, actual_values)
 
-    ans = []
-    rows = len(grid) - 1
-    columns = len(grid[0]) - 1
+    def test_solve(self):
+        grid = [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+        expected_solution = [
+            ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+            ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+            ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+            ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+            ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+            ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+            ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+            ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+            ["3", "4", "5", "2", "8", "6", "1", "7", "9"],
+        ]
+        actual_solution = sudoku.solve(grid)
+        self.assertEqual(expected_solution, actual_solution)
 
-    for i in range(columns):
-        if grid[0][i] == "X":
-            ans.append((0, i))
-    for i in range(rows):
-        if grid[i][0] == "X":
-            ans.append((i, 0))
+        grid = [
+            ["6", "6", "1", "1", "1", "5", "8", "3", "7"],
+            ["3", "5", "7", "8", "2", "6", "1", "4", "9"],
+            ["1", "4", "8", "9", "3", ".", ".", "2", "6"],
+            ["6", "3", "9", "5", "1", "2", "4", "7", "8"],
+            ["5", ".", "1", "7", "6", ".", "3", ".", "2"],
+            ["4", "7", "2", "3", ".", "8", "6", "1", "5"],
+            ["9", "6", "4", "2", "8", "3", "7", "5", "1"],
+            ["8", "1", ".", "4", "7", "9", "2", "6", "3"],
+            ["7", "2", ".", "6", "5", "1", "9", "8", "."],
+        ]
+        expected_solution = [
+            ["6", "6", "1", "1", "1", "5", "8", "3", "7"],
+            ["3", "5", "7", "8", "2", "6", "1", "4", "9"],
+            ["1", "4", "8", "9", "3", "7", "5", "2", "6"],
+            ["6", "3", "9", "5", "1", "2", "4", "7", "8"],
+            ["5", "8", "1", "7", "6", "4", "3", "9", "2"],
+            ["4", "7", "2", "3", "9", "8", "6", "1", "5"],
+            ["9", "6", "4", "2", "8", "3", "7", "5", "1"],
+            ["8", "1", "5", "4", "7", "9", "2", "6", "3"],
+            ["7", "2", "3", "6", "5", "1", "9", "8", "4"],
+        ]
+        actual_solution = sudoku.solve(grid)
+        self.assertEqual(expected_solution, actual_solution)
 
-    if len(ans) != 2:
-        for i in range(columns):
-            if grid[rows][i] == "X":
-                ans.append((rows, i))
-        for i in range(rows):
-            if grid[i][columns] == "X":
-                ans.append((i, columns))
-    if len(ans) > 1:
-        if ans[0][1] > ans[1][1]:
-            ans[0], ans[1] = ans[1], ans[0]
-        if ans[0][0] > ans[1][0]:
-            ans[0], ans[1] = ans[1], ans[0]
+    def test_check_solution(self):
+        good_solution = [
+            ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+            ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+            ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+            ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+            ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+            ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+            ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+            ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+            ["3", "4", "5", "2", "8", "6", "1", "7", "9"],
+        ]
+        self.assertTrue(sudoku.check_solution(good_solution))
 
-    return ans
+        not_solved = [
+            ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+            ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+            ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+            ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+            ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+            ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+            ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+            ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+            ["3", "4", "5", "2", "8", "6", "1", "7", "."],
+        ]
+        self.assertFalse(sudoku.check_solution(not_solved))
 
+        bad_solution = [
+            ["6", "6", "1", "1", "1", "5", "8", "3", "7"],
+            ["3", "5", "7", "8", "2", "6", "1", "4", "9"],
+            ["1", "4", "8", "9", "3", "7", "5", "2", "6"],
+            ["6", "3", "9", "5", "1", "2", "4", "7", "8"],
+            ["5", "8", "1", "7", "6", "4", "3", "9", "2"],
+            ["4", "7", "2", "3", "9", "8", "6", "1", "5"],
+            ["9", "6", "4", "2", "8", "3", "7", "5", "1"],
+            ["8", "1", "5", "4", "7", "9", "2", "6", "3"],
+            ["7", "2", "3", "6", "5", "1", "9", "8", "4"],
+        ]
+        self.assertFalse(sudoku.check_solution(bad_solution))
 
-def make_step(grid: List[List[Union[str, int]]], k: int) -> List[List[Union[str, int]]]:
-    """
+        bad_solution = [[str(v) for v in range(1, 10)]] * 9
+        self.assertFalse(sudoku.check_solution(bad_solution))
 
-    :param grid:
-    :param k:
-    :return:
-    """
+        bad_solution = [[str(v)] * 9 for v in range(1, 10)]
+        self.assertFalse(sudoku.check_solution(bad_solution))
 
-    moves = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    to_visit = []
+    def test_generate_sudoku(self):
+        grid = sudoku.generate_sudoku(40)
+        expected_unknown = 41
+        actual_unknown = sum(1 for row in grid for e in row if e == ".")
+        self.assertEqual(expected_unknown, actual_unknown)
+        solution = sudoku.solve(grid)
+        solved = sudoku.check_solution(solution)
+        self.assertTrue(solved)
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == k:
-                to_visit.append((i, j, k + 1))
-    while to_visit != []:
-        i, j = to_visit[0][0], to_visit[0][1]
-        for x, y in moves:
-            if 0 <= i + x < len(grid) and 0 <= j + y < len(grid[0]):
-                if grid[i + x][j + y] == 0:
-                    grid[i + x][j + y] = to_visit[0][2]
-        to_visit.pop(0)
-    return grid
+        grid = sudoku.generate_sudoku(1000)
+        expected_unknown = 0
+        actual_unknown = sum(1 for row in grid for e in row if e == ".")
+        self.assertEqual(expected_unknown, actual_unknown)
+        solution = sudoku.solve(grid)
+        solved = sudoku.check_solution(solution)
+        self.assertTrue(solved)
 
-
-def shortest_path(
-    grid: List[List[Union[str, int]]], exit_coord: Tuple[int, int]
-) -> Optional[Union[Tuple[int, int], List[Tuple[int, int]]]]:
-    """
-
-    :param grid:
-    :param exit_coord:
-    :return:
-    """
-    x = exit_coord[0]
-    y = exit_coord[1]
-    k = grid[x][y]
-    moves = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    way = []
-    way.append((x, y))
-    while k != 1:
-        for a, b in moves:
-            if 0 <= x + a < len(grid) and 0 <= y + b < len(grid[0]):
-                temp = grid[x + a][y + b]
-                if isinstance(temp, int):
-                    if temp < int(k):
-                        x, y = x + a, y + b
-                        way.append((x, y))
-                        k = grid[x][y]
-
-    for i in range(len(grid) - 1):
-        for j in range(len(grid[0])):
-            if grid[i][j] != "■":
-                grid[i][j] = " "
-
-    return way
-
-
-def encircled_exit(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) -> bool:
-    """
-
-    :param grid:
-    :param coord:
-    :return:
-    """
-
-    ans = False
-
-    if (
-        coord == (0, 0)
-        or coord == (len(grid) - 1, len(grid[0]) - 1)
-        or coord == (len(grid) - 1, 0)
-        or coord == (0, len(grid[0]) - 1)
-    ):
-        ans = True
-    elif coord[0] == 0:
-        if grid[1][coord[1]] != " ":
-            ans = True
-
-    elif coord[1] == 0:
-        if grid[coord[0]][1] != " ":
-            ans = True
-
-    elif coord[0] == len(grid) - 1:
-        if grid[len(grid) - 2][coord[1]] != " ":
-            ans = True
-
-    elif coord[1] == len(grid[0]) - 1:
-        if grid[coord[0]][len(grid[0]) - 2] != " ":
-            ans = True
-    return ans
-
-
-def solve_maze(
-    grid: List[List[Union[str, int]]],
-) -> Tuple[List[List[Union[str, int]]], Optional[Union[Tuple[int, int], List[Tuple[int, int]]]]]:
-    """
-
-    :param grid:
-    :return:
-    """
-
-    exits = get_exits(grid)
-    if len(exits) < 2:
-        return grid, exits[0]
-    else:
-        for exit in exits:
-            if encircled_exit(grid, exit):
-                return None, None  # type: ignore
-    enter = exits[0]
-    exit = exits[1]
-    if exit[1] - enter[1] == 1 and exit[0] - enter[0] == 0:
-        return grid, exits[::-1]
-    elif exit[1] - enter[1] == 0 and exit[0] - enter[0] == 1:
-        return grid, exits[::-1]
-    elif exit[0] - enter[0] == 0 and exit[1] - enter[1] == 1:
-        return grid, exits[::-1]
-    elif exit[0] - enter[0] == 1 and exit[1] - enter[1] == 0:
-        return grid, exits[::-1]
-
-    grid[exits[0][0]][exits[0][1]] = 1
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == " ":
-                grid[i][j] = 0
-            elif grid[i][j] == "X":
-                grid[i][j] = 0
-
-    k = 1
-    while grid[exits[1][0]][exits[1][1]] == 0:
-        grid = make_step(grid, k)
-        k += 1
-
-    path = shortest_path(grid, exits[1])
-
-    return grid, path
-
-
-def add_path_to_grid(
-    grid: List[List[Union[str, int]]], path: Optional[Union[Tuple[int, int], List[Tuple[int, int]]]]
-) -> List[List[Union[str, int]]]:
-    """
-
-    :param grid:
-    :param path:
-    :return:
-    """
-
-    if path:
-        for i, row in enumerate(grid):
-            for j, _ in enumerate(row):
-                if (i, j) in path:
-                    grid[i][j] = "X"
-    return grid
-
-
-if __name__ == "__main__":
-    print(pd.DataFrame(bin_tree_maze(15, 15)))
-    GRID = bin_tree_maze(15, 15)
-    print(pd.DataFrame(GRID))
-    _, PATH = solve_maze(GRID)
-    MAZE = add_path_to_grid(GRID, PATH)
-    print(pd.DataFrame(MAZE))
+        grid = sudoku.generate_sudoku(0)
+        expected_unknown = 81
+        actual_unknown = sum(1 for row in grid for e in row if e == ".")
+        self.assertEqual(expected_unknown, actual_unknown)
+        solution = sudoku.solve(grid)
+        solved = sudoku.check_solution(solution)
+        self.assertTrue(solved)
+        
